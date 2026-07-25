45c156efa9b7f13a3600818777a9d4d673cf1017
namunanamu <namunanamu76@gmail.com>
2026-07-26 08:47:57 +0900

    fix: 部屋参加の発火機構をダイアログに変更（仮）

--- src/client/ui/topRightUI.tsx
+++ src/client/ui/topRightUI.tsx
@@ -1,5 +1,3 @@
-import { socketController } from "../socket/SocketController";
-
 export const TopRightUI = (
     <button
         style = {{
@@ -10,8 +8,7 @@ export const TopRightUI = (
             fontWeight: "bold",
         }}
         onClick = {() => {
-            socketController.joinRoom("123"); // TODO: ルーム参加の仕組みを整える（UI作成してそれトリガーに発火）
-            alert("123に部屋参加");
+            
         }}
     >
     部屋参加
